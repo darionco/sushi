8df700b686e8f5a62f1d38d6074db3ecbb3f1d2b
Dario Segura <dsegura@league.com>
2022-04-24 13:51:58 -0400

    [FORGE] Logger uses standard `console` methods

--- src/utils/FSHLogger.ts
+++ src/utils/FSHLogger.ts
@@ -1,119 +1,127 @@
-import { createLogger, format, transports } from 'winston';
-import chalk from 'chalk';
+// import { createLogger, format, transports } from 'winston';
+// import chalk from 'chalk';
 import cloneDeep from 'lodash/cloneDeep';
 
-const { combine, printf } = format;
+// const { combine, printf } = format;
 
-const withLocation = format(info => {
-  if (info.file) {
-    info.message += `\n  File: ${info.file}`;
-    delete info.file;
-  }
-  if (info.location) {
-    info.message += `\n  Line: ${info.location.startLine}`;
-    if (info.location.endLine !== info.location.startLine) {
-      info.message += ` - ${info.location.endLine}`;
-    }
-    delete info.location;
-  }
-  if (info.appliedFile) {
-    info.message += `\n  Applied in File: ${info.appliedFile}`;
-    delete info.appliedFile;
-  }
-  if (info.appliedLocation) {
-    info.message += `\n  Applied on Line: ${info.appliedLocation.startLine}`;
-    if (info.appliedLocation.endLine !== info.appliedLocation.startLine) {
-      info.message += ` - ${info.appliedLocation.endLine}`;
-    }
-    delete info.appliedLocation;
-  }
-  return info;
-});
+// const withLocation = format(info => {
+//   if (info.file) {
+//     info.message += `\n  File: ${info.file}`;
+//     delete info.file;
+//   }
+//   if (info.location) {
+//     info.message += `\n  Line: ${info.location.startLine}`;
+//     if (info.location.endLine !== info.location.startLine) {
+//       info.message += ` - ${info.location.endLine}`;
+//     }
+//     delete info.location;
+//   }
+//   if (info.appliedFile) {
+//     info.message += `\n  Applied in File: ${info.appliedFile}`;
+//     delete info.appliedFile;
+//   }
+//   if (info.appliedLocation) {
+//     info.message += `\n  Applied on Line: ${info.appliedLocation.startLine}`;
+//     if (info.appliedLocation.endLine !== info.appliedLocation.startLine) {
+//       info.message += ` - ${info.appliedLocation.endLine}`;
+//     }
+//     delete info.appliedLocation;
+//   }
+//   return info;
+// });
+//
+// const ignoreWarnings = format(info => {
+//   // Only warnings can be ignored
+//   if (info.level !== 'warn') {
+//     return info;
+//   }
+//   const shouldIgnore = ignoredWarnings?.some(m => {
+//     return typeof m === 'string' ? m === info.message : m.test(info.message);
+//   });
+//   return shouldIgnore ? false : info;
+// });
+//
+// const incrementCounts = format(info => {
+//   switch (info.level) {
+//     case 'info':
+//       stats.numInfo++;
+//       break;
+//     case 'warn':
+//       stats.numWarn++;
+//       break;
+//     case 'error':
+//       stats.numError++;
+//       break;
+//     case 'debug':
+//       stats.numDebug++;
+//       break;
+//     default:
+//       break;
+//   }
+//
+//   return info;
+// });
+//
+// const trackErrorsAndWarnings = format(info => {
+//   if (!errorsAndWarnings.shouldTrack) {
+//     return info;
+//   }
+//   if (info.level === 'error') {
+//     errorsAndWarnings.errors.push({
+//       message: info.message,
+//       location: info.location,
+//       input: info.file
+//     });
+//   } else if (info.level === 'warn') {
+//     errorsAndWarnings.warnings.push({
+//       message: info.message,
+//       location: info.location,
+//       input: info.file
+//     });
+//   }
+//   return info;
+// });
+//
+// const printer = printf(info => {
+//   let level;
+//   switch (info.level) {
+//     case 'info':
+//       level = chalk.whiteBright.bgGreen(`${info.level} `);
+//       break;
+//     case 'warn':
+//       // (179, 98, 0) = dark dark orange
+//       level = chalk.whiteBright.bgRgb(179, 98, 0)(`${info.level} `);
+//       break;
+//     case 'error':
+//       level = chalk.whiteBright.bgRed(`${info.level}`);
+//       break;
+//     case 'debug':
+//       level = chalk.whiteBright.bgBlue(`${info.level}`);
+//       break;
+//     default:
+//       break;
+//   }
+//   return `${level} ${info.message}`;
+// });
 
-const ignoreWarnings = format(info => {
-  // Only warnings can be ignored
-  if (info.level !== 'warn') {
-    return info;
-  }
-  const shouldIgnore = ignoredWarnings?.some(m => {
-    return typeof m === 'string' ? m === info.message : m.test(info.message);
-  });
-  return shouldIgnore ? false : info;
-});
-
-const incrementCounts = format(info => {
-  switch (info.level) {
-    case 'info':
-      stats.numInfo++;
-      break;
-    case 'warn':
-      stats.numWarn++;
-      break;
-    case 'error':
-      stats.numError++;
-      break;
-    case 'debug':
-      stats.numDebug++;
-      break;
-    default:
-      break;
-  }
-
-  return info;
-});
-
-const trackErrorsAndWarnings = format(info => {
-  if (!errorsAndWarnings.shouldTrack) {
-    return info;
-  }
-  if (info.level === 'error') {
-    errorsAndWarnings.errors.push({
-      message: info.message,
-      location: info.location,
-      input: info.file
-    });
-  } else if (info.level === 'warn') {
-    errorsAndWarnings.warnings.push({
-      message: info.message,
-      location: info.location,
-      input: info.file
-    });
-  }
-  return info;
-});
-
-const printer = printf(info => {
-  let level;
-  switch (info.level) {
-    case 'info':
-      level = chalk.whiteBright.bgGreen(`${info.level} `);
-      break;
-    case 'warn':
-      // (179, 98, 0) = dark dark orange
-      level = chalk.whiteBright.bgRgb(179, 98, 0)(`${info.level} `);
-      break;
-    case 'error':
-      level = chalk.whiteBright.bgRed(`${info.level}`);
-      break;
-    case 'debug':
-      level = chalk.whiteBright.bgBlue(`${info.level}`);
-      break;
-    default:
-      break;
-  }
-  return `${level} ${info.message}`;
-});
-
-export const logger = createLogger({
-  format: combine(
-    ignoreWarnings(),
-    incrementCounts(),
-    trackErrorsAndWarnings(),
-    withLocation(),
-    printer
-  ),
-  transports: [new transports.Console()]
-});
+export const logger = {
+  level: 'none',
+  info: console.info,
+  error: console.error,
+  warn: console.warn,
+  debug: console.debug,
+  transports: [] as any[],
+};
+// createLogger({
+//   format: combine(
+//     ignoreWarnings(),
+//     incrementCounts(),
+//     trackErrorsAndWarnings(),
+//     withLocation(),
+//     printer
+//   ),
+//   transports: [new transports.Console()]
+// });
 
 let ignoredWarnings: (string | RegExp)[];
 export const setIgnoredWarnings = (messages: string): void => {
